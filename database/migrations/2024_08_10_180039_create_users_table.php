0cc1b4c5f7fec1e71858a69699e706becf459f73
Zoya <zoya.semenova@mail.ru>
2024-08-13 03:23:04 +0500

    fix

--- database/migrations/2024_08_10_180039_create_users_table.php
+++ database/migrations/2024_08_10_180039_create_users_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('password');
-            $table->string('photo');
+            $table->string('photo')->nullable();
             $table->timestamps();
         });
     }
